468ed0daf03e4598dcc17a0948d896f0b0726896
Nikunj Thakor <nikunjthakor2001@gmail.com>
2024-06-02 13:29:23 -0400

    Added Repo for Live

---
+++ Server/Models/movie.ts
@@ -0,0 +1,42 @@
+//Movie Model here
+
+import { Collection, Schema, model } from 'mongoose';
+
+// Movie interface 
+interface IMovie 
+{
+    movieID: string,
+    title: string,
+    studio: string,
+    genres: string[],
+    directors: string[],
+    writers: string[],
+    actors: string[],
+    year: number,
+    length: number,
+    shortDescription: string,
+    mpaRating: string,
+    criticsRating: number
+}
+
+// Movie schema 
+let movieSchema = new Schema<IMovie>
+({
+    movieID: String,
+    title: String,
+    studio: String,
+    genres: [String],
+    directors: [String],
+    writers: [String],
+    actors: [String],
+    year: Number,
+    length: Number,
+    shortDescription: String,
+    mpaRating: String,
+    criticsRating: Number
+});
+
+
+let Movie = model<IMovie>('Movie', movieSchema);
+
+export default Movie;
